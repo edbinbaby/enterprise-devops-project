b5f985c25590b07fbcce18a1c0a219552cda34f9
Ubuntu <ubuntu@ip-172-31-11-242.ap-south-1.compute.internal>
2026-07-15 06:56:17 +0000

    Configure Entity Framework and initial migration

---
+++ employee-api/EmployeeApi/Program.cs
@@ -0,0 +1,27 @@
+using EmployeeApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
